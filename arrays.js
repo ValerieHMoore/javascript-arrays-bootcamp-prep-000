61582e101d300eeb9626f5efeb85bf8a594374b8
Valerie Moore <periwinkle17@outlook.com>
2018-04-22 16:22:44 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,1 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
-
-var cities = ["New York", "San Francisco"]
- 
-["Philadelphia", ...cities] // ["Philadelphia", "New York", "San Francisco"]
- 
-cities // ["New York", "San Francisco"]
